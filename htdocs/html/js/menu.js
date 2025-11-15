f308a411f1938cdf416d1790fa6e535d85c630d1
apprenti2012 <munoz.yannis04@gmail.com>
2025-11-15 10:29:03 +0100

    Delete htdocs/html/js directory

--- htdocs/html/js/menu.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-const menuLinks = document.getElementById("menuLinks");
-
-menuBtn.addEventListener("click", () => {
-  menuLinks.classList.toggle("show");
-});
